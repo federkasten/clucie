dcc024d77aee85de03e2c7dd2f002201bb09928a
Takashi AOKI <federkasten@me.com>
2015-03-25 20:16:31 +0900

    enable analyzer mapping

--- src/clucie/analysis.clj
+++ src/clucie/analysis.clj
@@ -1,11 +1,13 @@
 (ns clucie.analysis
   (:import [org.apache.lucene.analysis.standard StandardAnalyzer StandardTokenizer StandardFilter]
-           [org.apache.lucene.analysis.core LowerCaseFilter StopFilter]
+           [org.apache.lucene.analysis.core KeywordAnalyzer LowerCaseFilter StopFilter]
            [org.apache.lucene.analysis.util CharArraySet]
+           [org.apache.lucene.analysis.cjk CJKAnalyzer]
            [org.apache.lucene.analysis.ngram NGramTokenFilter]
-           [org.apache.lucene.analysis Analyzer Analyzer$TokenStreamComponents]))
+           [org.apache.lucene.analysis Analyzer Analyzer$TokenStreamComponents]
+           [org.apache.lucene.analysis.miscellaneous PerFieldAnalyzerWrapper]))
 
-(defmacro make-analyzer
+(defmacro build-analyzer
   [tokenizer & filters]
   `(proxy [org.apache.lucene.analysis.Analyzer] []
      (createComponents [field-name#]
@@ -30,10 +32,26 @@
   ([stop-words]
    (StandardAnalyzer. (char-set stop-words))))
 
+(defn keyword-analyzer
+  ^Analyzer
+  []
+  (KeywordAnalyzer.))
+
 (defn ngram-analyzer
   [min-length max-length stop-words]
-  (make-analyzer StandardTokenizer
-                 (NGramTokenFilter. min-length max-length)
-                 (StandardFilter.)
-                 (LowerCaseFilter.)
-                 (StopFilter. (char-set stop-words))))
+  (build-analyzer StandardTokenizer
+                  (NGramTokenFilter. min-length max-length)
+                  (StandardFilter.)
+                  (LowerCaseFilter.)
+                  (StopFilter. (char-set stop-words))))
+
+(defn cjk-analyzer
+  ([]
+   (CJKAnalyzer.))
+  ([stop-words]
+   (CJKAnalyzer. (char-set stop-words))))
+
+(defn analyzer-mapping
+  [default mapping]
+  (PerFieldAnalyzerWrapper. default
+                            (into {} (map (fn [[k v]] [(name k) v]) mapping))))
