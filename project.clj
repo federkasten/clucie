b72b03b807de1f8b10150720a56374b1f570e53d
Takashi AOKI <federkasten@me.com>
2015-05-08 19:07:47 +0900

    bump up version of lucene

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "Clojure for the Lucene"
   :url "https://github.com/federkasten/clucie"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.lucene/lucene-core "5.0.0"]
-                 [org.apache.lucene/lucene-analyzers-common "5.0.0"]]
+                 [org.apache.lucene/lucene-core "5.1.0"]
+                 [org.apache.lucene/lucene-analyzers-common "5.1.0"]]
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :profiles {:1.5  {:dependencies [[org.clojure/clojure "1.5.0"]]}
