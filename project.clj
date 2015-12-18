d563fb6bf0f1cc9912549b6cce6bf6c5baa08219
atsuo yamada <yamada-git@tir.jp>
2015-12-18 20:16:16 +0900

    Bump up version of clojure and lucene

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject clucie "0.1.0-SNAPSHOT"
   :description "Clojure for the Lucene"
   :url "https://github.com/federkasten/clucie"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.lucene/lucene-core "5.1.0"]
-                 [org.apache.lucene/lucene-analyzers-common "5.1.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.apache.lucene/lucene-core "5.4.0"]
+                 [org.apache.lucene/lucene-analyzers-common "5.4.0"]]
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :profiles {:1.5  {:dependencies [[org.clojure/clojure "1.5.0"]]}
-             :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}})
+             :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}})
