16d04c1e8745d8eb511152ce929bb22999810eb6
Takashi AOKI <me@tak.sh>
2016-08-13 05:57:55 +0000

    set version to 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clucie "0.1.4"
+(defproject clucie "0.1.5-SNAPSHOT"
   :description "Clojure for the Lucene"
   :url "https://github.com/federkasten/clucie"
   :license {:name "Apache License, Version 2.0"
