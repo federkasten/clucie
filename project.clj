5e9f0a29204f73b84d5d756904a668ba643ea14f
Takashi AOKI <me@tak.sh>
2016-11-21 09:38:20 +0900

    upgrade lucene

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :min-lein-version "2.5.0"
-  :dependencies [[org.apache.lucene/lucene-core "6.2.1"]
-                 [org.apache.lucene/lucene-analyzers-common "6.2.1"]
-                 [org.apache.lucene/lucene-analyzers-kuromoji "6.2.1"]]
+  :dependencies [[org.apache.lucene/lucene-core "6.3.0"]
+                 [org.apache.lucene/lucene-analyzers-common "6.3.0"]
+                 [org.apache.lucene/lucene-analyzers-kuromoji "6.3.0"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [midje "1.8.3"]]
                    :plugins [[lein-cloverage "1.0.8"]
