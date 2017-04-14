ca78702f68af79f5204bde92486991c5abe802f1
Takashi AOKI <me@tak.sh>
2017-04-14 15:12:16 +0900

    upgrade deps

--- project.clj
+++ project.clj
@@ -4,12 +4,16 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :min-lein-version "2.5.0"
-  :dependencies [[org.apache.lucene/lucene-core "6.3.0"]
-                 [org.apache.lucene/lucene-analyzers-common "6.3.0"]
-                 [org.apache.lucene/lucene-analyzers-kuromoji "6.3.0"]]
+  :dependencies [[org.apache.lucene/lucene-core "6.5.0"]
+                 [org.apache.lucene/lucene-analyzers-common "6.5.0"]
+                 [org.apache.lucene/lucene-analyzers-icu "6.5.0"]
+                 [org.apache.lucene/lucene-analyzers-kuromoji "6.5.0"]
+                 [org.apache.lucene/lucene-analyzers-phonetic "6.5.0"]
+                 [org.apache.lucene/lucene-analyzers-smartcn "6.5.0"]
+                 [org.apache.lucene/lucene-analyzers-stempel "6.5.0"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [midje "1.8.3"]]
-                   :plugins [[lein-cloverage "1.0.8"]
+                   :plugins [[lein-cloverage "1.0.9"]
                              [lein-midje "3.2.1"]]
                    :global-vars {*warn-on-reflection* true
                                  *assert* true}}
