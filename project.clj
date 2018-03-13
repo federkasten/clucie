03fbf80f5ea1862f87c7824f3001c6242c3a0dde
Takashi AOKI <me@tak.sh>
2018-03-14 00:11:12 +0900

    upgrade minor version of lucene deps

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :min-lein-version "2.5.0"
-  :dependencies [[org.apache.lucene/lucene-core "6.6.1"]
-                 [org.apache.lucene/lucene-analyzers-common "6.6.1"]
-                 [org.apache.lucene/lucene-analyzers-icu "6.6.1"]
-                 [org.apache.lucene/lucene-analyzers-kuromoji "6.6.1"]
-                 [org.apache.lucene/lucene-analyzers-phonetic "6.6.1"]
-                 [org.apache.lucene/lucene-analyzers-smartcn "6.6.1"]
-                 [org.apache.lucene/lucene-analyzers-stempel "6.6.1"]
-                 [org.apache.lucene/lucene-queryparser "6.6.1"]]
+  :dependencies [[org.apache.lucene/lucene-core "6.6.3"]
+                 [org.apache.lucene/lucene-analyzers-common "6.6.3"]
+                 [org.apache.lucene/lucene-analyzers-icu "6.6.3"]
+                 [org.apache.lucene/lucene-analyzers-kuromoji "6.6.3"]
+                 [org.apache.lucene/lucene-analyzers-phonetic "6.6.3"]
+                 [org.apache.lucene/lucene-analyzers-smartcn "6.6.3"]
+                 [org.apache.lucene/lucene-analyzers-stempel "6.6.3"]
+                 [org.apache.lucene/lucene-queryparser "6.6.3"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [midje "1.9.1"]]
                    :plugins [[lein-cloverage "1.0.10"]
