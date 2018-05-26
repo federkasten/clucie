07f5899ff86e3aebcd6218b47d8a88ed932a7710
Takashi AOKI <me@tak.sh>
2018-05-26 19:06:39 +0900

    version 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clucie "0.3.3-SNAPSHOT"
+(defproject clucie "0.4.0"
   :description "Clojure for the Lucene"
   :url "https://github.com/federkasten/clucie"
   :license {:name "Apache License, Version 2.0"
